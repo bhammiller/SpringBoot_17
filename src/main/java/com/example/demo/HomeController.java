f23779a9cd3d78c72e8f773f8372eff16ed960dc
Brandon Hammiller <bdayton1985@gmail.com>
2018-02-12 15:55:04 -0500

    Finished Lesson 24

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -9,4 +9,9 @@ public class HomeController {
     public String index(){
         return "index";
     }
+
+    @RequestMapping("/secure")
+    public String secure(){
+        return "secure";
+    }
 }
--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -14,6 +14,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/","/css/**","/js/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
